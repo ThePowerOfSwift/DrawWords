90a3f342a1e3369778f6ddd375d647e8e32f492b
Leon Yue <4940748@qq.com>
2016-01-04 16:45:27 +0800

    initial

---
+++ DrawWords/DrawWords/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawWords
+//
+//  Created by xiaogu on 16/1/4.
+//  Copyright © 2016年 leon yue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawWords/DrawWords/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawWords
+//
+//  Created by xiaogu on 16/1/4.
+//  Copyright © 2016年 leon yue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
